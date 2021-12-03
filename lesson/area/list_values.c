0b125d3e9024b674ddd2d980906675d8d7df1e90
Wanzaz <77801991+Wanzaz@users.noreply.github.com>
2021-12-03 15:02:27 +0100

    Create list_values.c

---
+++ lesson/area/list_values.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void list_area(int a[], int n) //a[] = square brackets are empty, if you write something there, it is ignored
+{
+  int i;
+  for(i=0;i<n;i++)
+  {
+    printf("%d", a[i]);
+  }
+  return 0;
+}
+
+int main()
+{
+  return 0;
+}
