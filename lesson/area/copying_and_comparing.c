b547ae00cca03122402ffabd97c9f6e1b3a24079
Wanzaz <77801991+Wanzaz@users.noreply.github.com>
2021-12-03 14:39:46 +0100

    Create copying_and_comparing.c

---
+++ lesson/area/copying_and_comparing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//kopírování a porovnávání pole
+
+int main()
+{
+  int i, pole1[10], pole2[12]
+  
+  for(i=0;i<10;i++)
+    pole1[i] = pole2[i];
+  
+  //or
+  
+  for(i=0;i<10;i++)
+    if(pole1[i] != pole2[i])
+      break;
+}
