bbbd1cdf73f1ab07c8204f46f3f33adf61e17b66
Wanzaz <77801991+Wanzaz@users.noreply.github.com>
2021-12-03 14:45:21 +0100

    Update copying_and_comparing.c

--- lesson/area/copying_and_comparing.c
+++ lesson/area/copying_and_comparing.c
@@ -4,14 +4,18 @@
 
 int main()
 {
-  int i, pole1[10], pole2[12]
+  int i, pole1[10], pole2[12];
   
   for(i=0;i<10;i++)
+  {
     pole1[i] = pole2[i];
-  
+  }
   //or
   
   for(i=0;i<10;i++)
+  {
     if(pole1[i] != pole2[i])
       break;
+  }
+  return 0;
 }
