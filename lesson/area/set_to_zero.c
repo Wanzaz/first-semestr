8b5927fd19d28e6086c0c56dbce98baea9ab6f86
Wanzaz <77801991+Wanzaz@users.noreply.github.com>
2021-12-03 14:31:56 +0100

    Create set_to_zero.c

---
+++ lesson/area/set_to_zero.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define POCET 10
+
+//the elements of the area are never set to zero until you do that
+
+int main()
+{
+  int i, pole[POCET];
+  for(i=0;i<POCET;i+++)
+    pole[i] = 0;
+}
+
+/*
+or we use a definition with initialization
+int pole [POCET] = {0,0,0,0,0,0,0,0,0,0};
+*/
