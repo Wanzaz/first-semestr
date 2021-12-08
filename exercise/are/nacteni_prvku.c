33e9116f58eb60faa2502e7c580398291425c4d7
Wanzaz <77801991+Wanzaz@users.noreply.github.com>
2021-12-08 11:01:32 +0100

    Create nacteni_prvku.c

---
+++ exercise/are/nacteni_prvku.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void retrieve_area(int a[], int n) //n = current size of area, a[] = maximal size of area
+{
+  int i;
+  for(i=0;i<n;i++)
+  {
+    printf("Enter %d. element of area: ", i + 1);
+    scanf("%d", &a[i]);
+  }
+  return 0;
+}
+
+int main()
+{
+  return 0;
+}
