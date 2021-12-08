5fa8debd71e329e858701c4103212b16957a2fab
Wanzaz <77801991+Wanzaz@users.noreply.github.com>
2021-12-08 13:39:32 +0100

    Create nacteni_5_do_pole.c

---
+++ lesson/area/nacteni_5_do_pole.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//vloz do vsech prvku pole na 5
+
+void vloz_petky(int a[], int n)
+{
+  int i;
+  for(i=0,i<n,i++)
+  {
+    a[i]=5;
+  }
+}
+
+int main()
+{
+  printf("%d", vloz_petky());
+  return 0;
+}
