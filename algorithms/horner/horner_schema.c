d0a7760d88b87ce9f69d7176a8b3c4fd7f0efae7
Wanzaz <77801991+Wanzaz@users.noreply.github.com>
2022-01-19 11:08:39 +0100

    Update horner_schema.c

--- algorithms/horner/horner_schema.c
+++ algorithms/horner/horner_schema.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
+/*
+Hornerovo schéma
+Napiš funkci horner, která vezme pole koeficientů, počet koeficientů a číslo x (všechno to budu celá čísla) a vrátí funkční hodnotu této funkce v bodě x. Pokud se rovná 0, jedná se o kořen rovnice.
+Vyřeš pomocí funkce horner tuto rovnici:
+x4 + 3x3 – 35x2 – 39x + 70= 0
+Pomůcka: int koeficienty[5] = {1, 3, -35, -39, 70};
+Celé kořeny (pokud existují) jsou dělitelé absolutního členu (70). Vypiš si tedy možné kořeny a všechny vyzkoušej.
+Výstup:
+Hornerovo schema!
+Cislo 1 je koren.
+Cislo -1 neni koren.
+Cislo 2 neni koren.
+Atd…
+*/
+
 float Horner(int koef[], int n, float cislo)
 {
     float suma = 0.0;
