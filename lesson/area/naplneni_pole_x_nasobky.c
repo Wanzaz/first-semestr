7b5d30d5cdecbc1b556561a59981b84dd598b47a
Wanzaz <77801991+Wanzaz@users.noreply.github.com>
2022-01-22 08:27:46 +0100

    Update naplneni_pole_x_nasobky.c

--- lesson/area/naplneni_pole_x_nasobky.c
+++ lesson/area/naplneni_pole_x_nasobky.c
@@ -3,7 +3,7 @@
 void nasobky(int a[],int n,int x)
 {
   int i, x=4;
-  for(i=0,i<n,i++)
+  for(i=0;i<n;i++)
   {
     a[i]=x*i;
   }
@@ -11,5 +11,11 @@ void nasobky(int a[],int n,int x)
 
 int main()
 {
+  int n;
+  printf("Zadej pocet prvku pole: ");
+  scanf("%d", &n);
+  
+  int a[n];
+  nasobky(a,n,x)
   return 0;
 }
