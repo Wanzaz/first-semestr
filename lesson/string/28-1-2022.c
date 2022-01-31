53cc73605ab7a3dc96e6df952b4553c246179ff7
Wanzaz <ondra.pazourek@gmail.com>
2022-01-31 22:27:03 +0100

    changed

--- lesson/string/28-1-2022.c
+++ lesson/string/28-1-2022.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void nacteni(char a[], int n)
-{
-    int i;
-    for(i=0;i<n;i++)
-    {
-      printf("Enter %d. element of area: ", i + 1);
-      scanf("%d", &a[i]);
-    }
-}
-
-void vypis(char p[], int n)
-{
-  for (int i = 0; i < n; i++)
-  {
-    printf("%d ", p[i]);
-  }
-  printf("\n");
-}
-
 
 /*
 1.nadefinujte mi textovy retez ktery bude mit 50 znaku skola
